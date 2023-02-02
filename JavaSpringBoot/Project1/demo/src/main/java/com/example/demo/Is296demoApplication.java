c83b8be9188e2d109d2b8d2d5099de58c42d6257
Zach Karr <zacharykarr60@gmail.com>
2023-02-01 20:11:51 -0600

    new

---
+++ JavaSpringBoot/Project1/demo/src/main/java/com/example/demo/Is296demoApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class Is296demoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Is296demoApplication.class, args);
+		System.out.print("\nHello\n");
+	}
+
+}
