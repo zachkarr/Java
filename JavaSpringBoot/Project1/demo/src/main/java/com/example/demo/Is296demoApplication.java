22539722b1d51411b0bb665cb94d91dad4053eb2
Zach Karr <zacharykarr60@gmail.com>
2023-02-01 19:52:32 -0600

    New

---
+++ JavaSpringBoot/Project1/demo/src/main/java/com/example/demo/Is296demoApplication.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Is296demoApplication {
+	public static void main(String[] args) {
+		System.out.print("Hello, World!");
+	}
+
+}
--- JavaSpringBoot/Project1/demo/src/main/java/config/DemoConfig.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-import org.springframework.context.annotation.Bean;
-public class DemoConfig {
-
-}
-
